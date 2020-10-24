9b1e4fe8a4581aa9a8a9f9d17d2df2adf218ada2
rohit nagappa <rnagappa35@gmail.com>
2020-10-24 16:36:25 +0530

    added export array in core module

---
+++ src/app/core/core-driective.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CoreDriectiveDirective } from './core-driective.directive';
+
+describe('CoreDriectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CoreDriectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { CorePipePipe } from './core-pipe.pipe';
+import { CoreDriectiveDirective } from './core-driective.directive';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, CorePipePipe],
+  declarations: [HeaderComponent, CorePipePipe, CoreDriectiveDirective],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: []
 })
 export class CoreModule { }
